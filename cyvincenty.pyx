8766d8e529a609fdb234a8fe3f6459a9448f9e2f
Daniel Sullivan <sullydm@gmail.com>
2018-02-09 13:32:42 -0500

    Optimize for cython

--- cyvincenty.pyx
+++ cyvincenty.pyx
@@ -1,42 +1,69 @@
 from geopy.distance import vincenty as v1
 
-import math
+from libc.math cimport sin, cos, tan, atan, sqrt, atan2, pi
 
 # WGS 84
-a = 6378137  # meters
-f = 1 / 298.257223563
-b = 6356752.314245  # meters; b = (1 - f)a
+cdef double  a = 6378137  # meters
+cdef double  f = 1 / 298.257223563
+cdef double  b = 6356752.314245  # meters; b = (1 - f)a
 
-MILES_PER_KILOMETER = 0.621371
-
-MAX_ITERATIONS = 200
-CONVERGENCE_THRESHOLD = 1e-12  # .000,000,000,001
+cdef int MAX_ITERATIONS = 200
+cdef double  CONVERGENCE_THRESHOLD = 1e-12  # .000,000,000,001
 
 
-def vincenty(ay, ax, by, bx):
+cpdef double vincenty(double ax, double ay, double bx, double by):
+
+    cdef int iteration
+    cdef double U1
+    cdef double U2
+    cdef double L
+    cdef double Lambda
+    cdef double sinU1
+    cdef double sinU2
+    cdef double cosU1
+    cdef double cosU2
+
+    cdef double sinLambda
+    cdef double cosLambda
+    cdef double sinSigma
+    cdef double cosSigma
+    cdef double sigma
+    cdef double sinAlpha
+    cdef double cosSqAlpha
+    cdef double cos2SigmaM
+    cdef double C
+    cdef double LambdaPrev
+    cdef double lambda_diff
+
+    cdef double uSq
+    cdef double A
+    cdef double B
+    cdef double deltaSigma
+    cdef double s
+
     # short-circuit coincident points
     if ay == by and ax == bx:
         return 0.0
 
-    U1 = math.atan((1 - f) * math.tan(math.radians(ay)))
-    U2 = math.atan((1 - f) * math.tan(math.radians(by)))
-    L = math.radians(bx - ax)
+    U1 = atan((1 - f) * tan(radians(ay)))
+    U2 = atan((1 - f) * tan(radians(by)))
+    L = radians(bx - ax)
     Lambda = L
 
-    sinU1 = math.sin(U1)
-    cosU1 = math.cos(U1)
-    sinU2 = math.sin(U2)
-    cosU2 = math.cos(U2)
+    sinU1 = sin(U1)
+    cosU1 = cos(U1)
+    sinU2 = sin(U2)
+    cosU2 = cos(U2)
 
     for iteration in range(MAX_ITERATIONS):
-        sinLambda = math.sin(Lambda)
-        cosLambda = math.cos(Lambda)
-        sinSigma = math.sqrt((cosU2 * sinLambda) ** 2 +
+        sinLambda = sin(Lambda)
+        cosLambda = cos(Lambda)
+        sinSigma = sqrt((cosU2 * sinLambda) ** 2 +
                              (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) ** 2)
         if sinSigma == 0:
             return 0.0  # coincident points
         cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda
-        sigma = math.atan2(sinSigma, cosSigma)
+        sigma = atan2(sinSigma, cosSigma)
         sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma
         cosSqAlpha = 1 - sinAlpha ** 2
         if cosSqAlpha == 0:
@@ -48,10 +75,13 @@ def vincenty(ay, ax, by, bx):
         Lambda = L + (1 - C) * f * sinAlpha * (sigma + C * sinSigma *
                                                (cos2SigmaM + C * cosSigma *
                                                 (-1 + 2 * cos2SigmaM ** 2)))
-        if abs(Lambda - LambdaPrev) < CONVERGENCE_THRESHOLD:
+        lambda_diff = Lambda - LambdaPrev
+        if lambda_diff < 0:
+            lambda_diff = -1 * lambda_diff
+        if lambda_diff < CONVERGENCE_THRESHOLD:
             break  # successful convergence
     else:
-        return None  # failure to converge
+        return -1  # failure to converge
 
     uSq = cosSqAlpha * (a ** 2 - b ** 2) / (b ** 2)
     A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)))
@@ -66,9 +96,5 @@ def vincenty(ay, ax, by, bx):
     return s
 
 
-if __name__ == '__main__':
-    p1 = (34, -118)
-    p2 = (35, -117)
-    new = vincenty(p1[0], p1[1], p2[0], p2[1])
-    old = v1(p1, p2).km
-    print(new / old)
+cdef double radians(double x):
+    return x * pi / 180
--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from cyvincenty import vincenty as v2
 if __name__ == '__main__':
     p1 = (34, -118)
     p2 = (35, -117)
-    new = v2(p1[0], p1[1], p2[0], p2[1])
+    new = v2(p1[1], p1[0], p2[1], p2[0])
     old = v1(p1, p2).km
     print(new / old)
